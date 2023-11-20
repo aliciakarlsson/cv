95e00919931481753874819a37b22d8d2a2d5ccd
Alicia <aliciaxd@live.se>
2023-11-20 11:15:57 +0100

    changed link from bilder to Bilder bc of typo

--- cv.js
+++ cv.js
@@ -57,17 +57,22 @@ async function getData() {
 getData();
 
 // Dark theme
+
+//Hämtar variablerna från DOM
 const toggle = document.getElementById("theme-toggle");
 const sunIcon = document.getElementById("sunicon");
-const moonIcon = document.getElementById("moonicon");
+const moonIcon = document.getElementById('moonicon');
 
+//Kollar om det finns lagrat tema eller använder standardtemat
 const storedTheme =
   localStorage.getItem("theme") ||
   (window.matchMedia("(prefers-color-scheme: dark)").matches
     ? "dark"
     : "light");
 
-if (storedTheme) {
+// OM det finns lagrat tema gör detta
+ if (storedTheme) {
+  //Sätt det lagrade temat till dokumentet
   document.documentElement.setAttribute("data-theme", storedTheme);
   if (storedTheme === "dark") {
     sunIcon.style.display = "none"; // Dölj solen i dark mode
@@ -78,10 +83,12 @@ if (storedTheme) {
   }
 }
 
+//Sätter eventlistener på det som växlar tema
 toggle.onclick = function () {
+  //Hämtar temat
   var currentTheme = document.documentElement.getAttribute("data-theme");
   var targetTheme = "light";
-
+// Om det är light, byt till dark
   if (currentTheme === "light") {
     targetTheme = "dark";
     sunIcon.style.display = "none"; // Dölj solen när du byter till dark mode
@@ -90,7 +97,7 @@ toggle.onclick = function () {
     sunIcon.style.display = "block"; // Visa solen när du byter till light mode
     moonIcon.style.display = "none"; // Dölj månen när du byter till light mode
   }
-
+//Sätter temat till DOMens attribut och sparar det
   document.documentElement.setAttribute("data-theme", targetTheme);
   localStorage.setItem("theme", targetTheme);
 };
--- portfolio.js
+++ portfolio.js
@@ -1,15 +1,20 @@
 // Dark theme
+
+//Hämtar variablerna från DOM
 const toggle = document.getElementById("theme-toggle");
 const sunIcon = document.getElementById("sunicon");
 const moonIcon = document.getElementById("moonicon");
 
+//Kollar om det finns lagrat tema eller använder standardtemat
 const storedTheme =
   localStorage.getItem("theme") ||
   (window.matchMedia("(prefers-color-scheme: dark)").matches
     ? "dark"
     : "light");
 
+// OM det finns lagrat tema gör detta
 if (storedTheme) {
+  //Sätt det lagrade temat till dokumentet
   document.documentElement.setAttribute("data-theme", storedTheme);
   if (storedTheme === "dark") {
     sunIcon.style.display = "none"; // Dölj solen i dark mode
@@ -20,10 +25,12 @@ if (storedTheme) {
   }
 }
 
+//Sätter eventlistener på det som växlar tema
 toggle.onclick = function () {
+  //Hämtar temat
   var currentTheme = document.documentElement.getAttribute("data-theme");
   var targetTheme = "light";
-
+  // Om det är light, byt till dark
   if (currentTheme === "light") {
     targetTheme = "dark";
     sunIcon.style.display = "none"; // Dölj solen när du byter till dark mode
@@ -32,7 +39,7 @@ toggle.onclick = function () {
     sunIcon.style.display = "block"; // Visa solen när du byter till light mode
     moonIcon.style.display = "none"; // Dölj månen när du byter till light mode
   }
-
+  //Sätter temat till DOMens attribut och sparar det
   document.documentElement.setAttribute("data-theme", targetTheme);
   localStorage.setItem("theme", targetTheme);
 };
