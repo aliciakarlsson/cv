f9a7409a7f0ff11e95feb657510e1a3e233f0fb1
Alicia <aliciaxd@live.se>
2023-11-28 11:01:28 +0100

    form gets empty when pressed send now

--- js/kontakt.js
+++ js/kontakt.js
@@ -1,1 +1,12 @@
+const submitBtn = document.getElementById('submitbutton');
+let emailInput = document.getElementById('writehere');
+let nameInput = document.getElementById('writehere2');
+let subjectInput = document.getElementById('subject');
+
+submitBtn.addEventListener('click', function(){
+
+    emailInput.value = '';
+    nameInput.value = '';
+    subjectInput.value = '';
+});
 
