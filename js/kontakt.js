b9d244178823bede1d96caa7e7fc06f2f5c94a2b
Alicia <aliciaxd@live.se>
2023-11-29 10:07:32 +0100

    added a prevent default to not reload page

--- js/kontakt.js
+++ js/kontakt.js
@@ -3,7 +3,9 @@ let emailInput = document.getElementById('writehere');
 let nameInput = document.getElementById('writehere2');
 let subjectInput = document.getElementById('subject');
 
-submitBtn.addEventListener('click', function(){
+submitBtn.addEventListener('click', function(event){
+
+    event.preventDefault();
 
     emailInput.value = '';
     nameInput.value = '';
