0a3f6f7467828993419407d73a51e8562d8b9abb
Alicia <aliciaxd@live.se>
2024-01-30 10:37:49 +0100

    checking gtag again

--- js/main.js
+++ js/main.js
@@ -1,10 +1,9 @@
 const myCvButton = document.getElementById('cvbutton');
 
 myCvButton.addEventListener('click', function(){
-    gtag("event", "min_cv_knapp", {
-      event_category: "cv",
-      event_label: "etikett",
-      value: Number,
+    gtag("event", "cv_click", {
+      event_category: "Knappar",
+      event_label: "CV-knapp",
     });
 });
 
